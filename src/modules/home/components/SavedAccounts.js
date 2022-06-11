5e85257d0cb1e58c855384307478bb68a935b2c5
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-06-11 17:50:45 +0530

    Account addition component changes

--- src/modules/home/components/SavedAccounts.js
+++ src/modules/home/components/SavedAccounts.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Divider } from "semantic-ui-react";
 
 const SavedAccounts = ({ accounts, navigateTo, onDeleteAccClick }) => {
     return (
         <>
             <h2 className="heading">Welcome aboard explorer</h2>
             <p>Click your preferred account to view details.</p>
-            <div className="saved_accounts_grid">
-                {Object.keys(accounts).map((currAccount, index) => (
-                    <div key={currAccount} className="account_btn">
-                        <div className="acc_name" onClick={() => navigateTo(currAccount)}>
-                            {accounts[currAccount]}
+            <Divider />
+            <div className="saved_accounts_container">
+                <div className="table_header">Your saved accounts: </div>
+                <div className="accounts_grid">
+                    {Object.keys(accounts).map((currAccount, index) => (
+                        <div key={currAccount} className="account_btn">
+                            <div className="acc_name" onClick={() => navigateTo(currAccount)}>
+                                {accounts[currAccount]}
+                            </div>
+                            {/* <Icon name="close" className="icon-close" onClick={() => onDeleteAccClick(currAccount)} /> */}
                         </div>
-                        {/* <Icon name="close" className="icon-close" onClick={() => onDeleteAccClick(currAccount)} /> */}
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </>
     );

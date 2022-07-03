25d2fbb4ec7da14783e43a25a865d2b782aa0a02
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-07-03 12:33:05 +0530

    Updated date picker code

--- src/modules/airdrops/airdrops.js
+++ src/modules/airdrops/airdrops.js
@@ -1,27 +1,71 @@
-import React from "react";
-import DatePicker from "react-date-picker";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Divider, Image, Pagination } from "semantic-ui-react";
+import DatePicker from "react-date-picker";
+
 import XPTLogoImg from "../../assets/svg/xpt.svg";
 import { ROUTES } from "../../constants/common.constants";
+import { ApiCall } from "../../utils/api.util";
 import useMergedState from "../../utils/useMergedState";
 
 import "./airdrops.scss";
 
 const Airdrops = () => {
+    const navigate = useNavigate();
     const [state, setState] = useMergedState({
         date: new Date(),
+        activePage: 1,
+        totalPages: 0,
+        totalCount: 0,
+        limit: 4,
+        loading: true,
+        list: [],
     });
+    const { date, activePage, totalPages, limit, list } = state;
 
-    const navigate = useNavigate();
+    useEffect(() => {
+        fetchAirdropList(activePage, date);
+    }, []);
 
-    const { date } = state;
+    const fetchAirdropList = (pageNumber, date) => {
+        const payload = {
+            method: "GET",
+            url: "airdrop/list",
+            encrypt: true,
+            params: {
+                limit,
+                pageNumber,
+                date: date.getTime()
+            }
+        };
 
-    const onDateChange = (value) => setState({ date: value });
+        setState({ loading: true });
+        ApiCall(payload)
+            .then((response) => {
+                if (response.data) {
+                    const { totalCount, list } = response.data;
+                    setState({
+                        totalPages: Math.ceil(totalCount / 4),
+                        totalCount,
+                        list,
+                    });
+                }
+            })
+            .finally(() => {
+                setState({ loading: false });
+            });
+    }
 
-    const onDropClick = (id) => {
-        navigate(ROUTES.DROP_DETAILS.replace(":id", id));
-    };
+    const onDateChange = (value) => {
+        setState({ date: value });
+        if (value) {
+            fetchAirdropList(activePage, value);
+        };
+    }
+    const handlePaginationChange = (e, { activePage }) => {
+        setState({ activePage });
+        fetchAirdropList(activePage, date);
+    }
 
     return (
         <div className="airdrops_container">
@@ -46,28 +90,19 @@ const Airdrops = () => {
                     />
                 </div>
                 <div className="airdrops_container">
-                    <div className="airdrops">
-                        <div className="drop" onClick={() => onDropClick("a")}>
-                            <div className="left_section">Logo</div>
-                            <div className="heading">A</div>
-                        </div>
-                        <div className="drop">
-                            <div className="left_section">Logo</div>
-                            <div className="heading">A</div>
-                        </div>
-                        <div className="drop">
-                            <div className="left_section"></div>
-                            <div className="right_section">
-                                <div className="heading">A</div>
-                            </div>
-                        </div>
-                        <div className="drop">
-                            <div className="left_section">Logo</div>
-                            <div className="heading">A</div>
-                        </div>
-                    </div>
+                    <AirdropList {...{ list, navigate }} />
                     <div className="pagination">
-                        <Pagination defaultActivePage={1} firstItem={null} lastItem={null} pointing secondary totalPages={3} />
+                        <Pagination
+                            pointing
+                            secondary
+                            boundaryRange={0}
+                            defaultActivePage={1}
+                            ellipsisItem={null}
+                            firstItem={null}
+                            lastItem={null}
+                            siblingRange={1}
+                            totalPages={totalPages}
+                            onPageChange={handlePaginationChange} />
                     </div>
                 </div>
             </div>
@@ -76,3 +111,25 @@ const Airdrops = () => {
 };
 
 export default Airdrops;
+
+const AirdropList = ({ list, navigate }) => {
+    if (!list || list.length === 0) {
+        return null;
+    };
+
+    const onDropClick = (id) => {
+        navigate(ROUTES.DROP_DETAILS.replace(":id", id));
+    };
+
+    return (
+        <div className="airdrops">
+            {list.map(({ projectName }) => (
+                <div className="drop" onClick={() => onDropClick("a")}>
+                    <div className="left_section">Logo</div>
+                    <div className="heading">{projectName}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+

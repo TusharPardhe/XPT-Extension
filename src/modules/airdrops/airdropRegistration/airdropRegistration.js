99561dc9d917b3e156950dd7285d6b2a448d022c
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-07-11 00:06:49 +0530

    Airdrop code refactor

--- src/modules/airdrops/airdropRegistration/airdropRegistration.js
+++ src/modules/airdrops/airdropRegistration/airdropRegistration.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from "react-toastify";
-import { Button, Divider, Image, Input, Select, TextArea } from 'semantic-ui-react';
+import { Divider, Image } from 'semantic-ui-react';
 import { Client, convertHexToString } from 'xrpl';
-import DateTimePicker from 'react-datetime-picker';
 
 import useMergedState from '../../../utils/useMergedState';
+
+import { RegistrationForm } from './RegistrationForm';
 import XPTLogoImg from "../../../assets/svg/xpt.svg";
 import AnimatedLoader from '../../../components/AnimatedLoader/AnimatedLoader';
 import { PUBLIC_SERVER, ROUTES } from '../../../constants/common.constants';
@@ -193,165 +194,41 @@ const AirdropRegistration = () => {
 
     return (
         <div className="airdrop_registration_component">
-            <div className="heading_component">
-                <div className="heading" onClick={gotoAirdrops}>
-                    AIRDR
-                    <Image src={XPTLogoImg} className="logo_img" />
-                    PS
-                </div>
-                <div className="sub_heading">Never let a drop go unnoticed.</div>
-            </div>
+            <RegistrationHeading {...{ gotoAirdrops }} />
             <Divider />
-            {!isAnIssuer && (
-                <>
-                    <div className="not_issuer_not">
-                        <span>Restricted:</span> Only token issuer accounts can request for an airdrop listing. Please login using an issuer account.
-                    </div>
-                    <Divider />
-                </>
-            )}
-            <div className="registration_box">
-                <div className="heading">Please enter details:</div>
-                <div className="inputs">
-                    <div className="input_field">
-                        <div className="label">Select Token Ticker:</div>
-                        <Select
-                            placeholder="Select your token"
-                            options={ticker.options}
-                            error={ticker.error.length > 0}
-                            style={{ width: "100%" }}
-                            value={ticker.value}
-                            name="ticker"
-                            onChange={handleUserInput}
-                        />
-                    </div>
-                    <div className="input_field">
-                        <div className="label">Project Name:</div>
-                        <Input
-                            placeholder="Enter name here"
-                            style={{ width: "100%" }}
-                            value={projectName.value}
-                            name="projectName"
-                            onChange={handleUserInput}
-                            error={projectName.error.length > 0}
-                        />
-                    </div>
-                    <div className="input_field">
-                        <div className="label">Currency Name:</div>
-                        <Input
-                            placeholder="Enter currency name here"
-                            style={{ width: "100%" }}
-                            value={currencyName.value}
-                            name="currencyName"
-                            onChange={handleUserInput}
-                            error={currencyName.error.length > 0}
-                        />
-                    </div>
-                    <div className="input_field">
-                        <div className="label">Airdrop Date:</div>
-                        <DateTimePicker
-                            onChange={(value) => setState({ date: { value } })}
-                            value={date.value}
-                            style={{ width: "100%" }}
-                            required
-                        />
-                    </div>
-                    <div className="input_field">
-                        <div className="label">Links:</div>
-                        <div className='social_inputs'>
-                            <Input
-                                label={{ content: "Logo: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={logo.value}
-                                name="logo"
-                                placeholder="Png/Jpeg/Jpg link"
-                                onChange={handleUserInput}
-                                error={logo.error.length > 0}
-                            />
-                            <Input
-                                label={{ content: "Twitter: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={twitter.value}
-                                name="twitter"
-                                placeholder="Enter twitter link"
-                                onChange={handleUserInput}
-                            />
-                            <Input
-                                label={{ content: "Discord: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={discord.value}
-                                name="discord"
-                                placeholder="Enter discord link"
-                                onChange={handleUserInput}
-                            />
-                            <Input
-                                label={{ content: "Website: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={website.value}
-                                name="website"
-                                placeholder="Enter website link"
-                                onChange={handleUserInput}
-                            />
-                            <Input
-                                label={{ content: "LinkTree: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={linktree.value}
-                                name="linktree"
-                                placeholder="Enter linktree link"
-                                onChange={handleUserInput}
-                            />
-                            <Input
-                                label={{ content: "Others: " }}
-                                labelPosition="left"
-                                style={{ width: "100%" }}
-                                value={others.value}
-                                name="others"
-                                placeholder="Enter additional links"
-                                onChange={handleUserInput}
-                            />
-                        </div>
-                    </div>
-                    <div className="input_field">
-                        <div className="label">Description:</div>
-                        <TextArea
-                            style={{
-                                width: "100%",
-                                minWidth: "100%",
-                                maxWidth: "100%",
-                                minHeight: "80px",
-                                maxHeight: "150px"
-                            }}
-                            name="description"
-                            onChange={handleUserInput}
-                            value={description.value}
-                            className={description.error.length > 0 ? "error_textarea" : ""}
-                        />
-                    </div>
-                </div>
-                <div className="submission_note">
-                    <span>Note:</span> A token issuer can submit only one request per issued token. If you want to omit your details after submission, please contact us on <a href=" ">Twitter</a>.
-                </div>
-                <div className="btn_container">
-                    <Button
-                        inverted
-                        color={"green"}
-                        type="submit"
-                        onClick={onSubmit}
-                        disabled={false}
-                        loading={loading}
-                    >
-                        Submit
-                    </Button>
-                </div>
-            </div>
+            <RestrictionNote show={!isAnIssuer} />
+            <RegistrationForm {...{ state, setState, handleUserInput, onSubmit }} />
             <AnimatedLoader loadingText="Fetching details..." isActive={loading} />
         </div>
     );
 }
 
 export default AirdropRegistration;
+
+
+const RegistrationHeading = ({ gotoAirdrops }) => {
+    return (
+        <div className="heading_component">
+            <div className="heading" onClick={gotoAirdrops}>
+                AIRDR
+                <Image src={XPTLogoImg} className="logo_img" />
+                PS
+            </div>
+            <div className="sub_heading">Never let a drop go unnoticed.</div>
+        </div>
+    );
+}
+
+const RestrictionNote = (show) => {
+    if (!show) return null;
+
+    return (
+        <>
+            <div className="not_issuer_not">
+                <span>Restricted:</span> Only token issuer accounts can request for an airdrop listing. Please login using an issuer account.
+            </div>
+            <Divider />
+        </>
+    );
+}
+
